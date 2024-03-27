3fce264577929d0f77a60311bc095803cbc6cae0
yujin.lee <yjlee@aitstory.com>
2024-03-27 13:49:58 +0900

    init

---
+++ src/main/java/com/nns/thinner/ThinnerApplication.java
@@ -0,0 +1,13 @@
+package com.nns.thinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nns/thinner/ThinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nns.thinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
