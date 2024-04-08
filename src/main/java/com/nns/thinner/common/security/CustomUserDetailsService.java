547c7f9cfdc2907edf7453fa456d43e916b65f98
yujin.lee <dbwksl128@gmail.com>
2024-04-08 17:28:17 +0900

    feat : Spring security userDetails 작업 중

---
+++ src/main/java/com/nns/thinner/common/security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.nns.thinner.common.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		return null;
+	}
+}
