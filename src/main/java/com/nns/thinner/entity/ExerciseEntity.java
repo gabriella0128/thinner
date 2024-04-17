f531a921bbe6de08fde8dba4a9d470fdf37165a3
yujin.lee <dbwksl128@gmail.com>
2024-04-17 13:09:28 +0900

    feat : exercise entity 추가

--- src/main/java/com/nns/thinner/entity/ExerciseEntity.java
+++ src/main/java/com/nns/thinner/entity/ExerciseEntity.java
@@ -1,4 +1,32 @@
 package com.nns.thinner.entity;
 
-public class ExerciseEntity {
+import org.hibernate.annotations.SQLRestriction;
+
+import com.nns.thinner.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "exercise")
+@SuperBuilder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("use_yn = true")
+public class ExerciseEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "exercise_idx")
+	private Long exerciseIdx;
 }
