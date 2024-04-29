738878f982cc776b9823a6ed3aab39070f3bacac
yujin.lee <dbwksl128@gmail.com>
2024-04-30 08:55:19 +0900

    feat : sendTempPassword 작업 중

--- src/main/java/com/nns/thinner/service/UserPasswordService.java
+++ src/main/java/com/nns/thinner/service/UserPasswordService.java
@@ -1,9 +1,11 @@
 package com.nns.thinner.service;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.nns.thinner.dto.UserPasswordChangeDto;
+import com.nns.thinner.dto.UserPasswordTempDto;
 import com.nns.thinner.dto.base.UserDto;
 import com.nns.thinner.service.dtoService.UserService;
 
@@ -20,10 +22,24 @@ public class UserPasswordService {
 
 	public UserPasswordChangeDto.Response changePassword(UserPasswordChangeDto.Request request) {
 		UserDto.Info user = userService.findByUserId(request.getUserId());
-
-		userService.save(user.toBuilder().password(passwordEncoder.encode(request.getNewPassword())).build());
-
+		if (passwordEncoder.matches(user.getPassword(), request.getOldPassword())) {
+			userService.save(user.toBuilder().password(passwordEncoder.encode(request.getNewPassword())).build());
+		} else {
+			throw new BadCredentialsException(user.getUserId());
+		}
 		return UserPasswordChangeDto.Response.builder().result(true).reason("SUCCESS").build();
 
 	}
+
+	public UserPasswordTempDto.Response sendTempPassword(UserPasswordTempDto.Request request) {
+		UserDto.Info user = userService.findByUserId(request.getUserId());
+		String tempPassword = generateTempPassword();
+
+		return UserPasswordTempDto.Response.builder().result(true).reason("SUCCESS").build();
+	}
+
+	public String generateTempPassword() {
+		return "";
+	}
+
 }
